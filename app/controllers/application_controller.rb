ab050d0a29f46a2b882a90a956c1853e59a6b156
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-09 21:49:22 -0500

    Add i18n (#1)
    * set locale from http header
    
    * set up I18n locales translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = extract_locale_from_accept_language_header.to_sym
+    validate_locale(locale)
+    locale = I18n.locale unless locale_supported?(locale)
+    logger.debug "* Locale set to '#{locale}'"
+    I18n.with_locale(locale, &action)
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
+  def validate_locale(locale)
+    if locale_supported?(locale)
+      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    else
+      logger.debug "* Language-Not-Supported: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    end
+  end
+
+  def locale_supported?(locale)
+    I18n.available_locales.include? locale
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module AirbnbClone
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Where the I18n library should search for translation files
+    # I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    # Permitted locales available for the application
+    I18n.available_locales = %i[en es]
+    # Set default locale to something other than :en
+    I18n.default_locale = :en
   end
 end
