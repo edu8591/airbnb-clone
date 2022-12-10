05a050db1a7adb12d683de2a995a7e290a2d56c0
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-09 23:40:37 -0500

    initialize rspc, fix application controller to correctly set locale (#2)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,12 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  around_action :use_locale
 
-  def switch_locale(&action)
-    locale = extract_locale_from_accept_language_header.to_sym
-    validate_locale(locale)
-    locale = I18n.locale unless locale_supported?(locale)
-    logger.debug "* Locale set to '#{locale}'"
-    I18n.with_locale(locale, &action)
+  def use_locale(&block)
+    cookies.permanent[:locale] = cookies[:locale] || I18n.default_locale
+    I18n.with_locale(cookies_locale, &block)
   end
 
-  private
-
-  def extract_locale_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-  end
-
-  def validate_locale(locale)
-    if locale_supported?(locale)
-      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-    else
-      logger.debug "* Language-Not-Supported: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-    end
-  end
-
-  def locale_supported?(locale)
-    I18n.available_locales.include? locale
+  def cookies_locale
+    @cookies_locale ||= cookies[:locale].to_sym
   end
 end
