afcef9b7dae67c679cebe2f5c5ca4aa513d9224e
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-28 12:36:01 -0500

    refactor Api::V1::UsersController to rescue from error, finish writing tests for api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,18 @@ module Api
   module V1
     class UsersController < ApplicationController
       def show
-        # puts User.find_by_email(params[:email])
-        render json: { user: User.find_by_email(params[:email]) }
+        user = User.find(params[:email])
+        respond_to do |format|
+          format.json {
+            render json: user.to_json, status: :ok
+          }
+        end
+      rescue ActiveRecord::RecordNotFound => e
+        respond_to do |format|
+          format.json {
+            render json: { error: e.message }.to_json, status: 404
+          }
+        end
       end
     end
   end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users', type: :request do
   describe "GET /show" do
+    let(:headers) do
+      { 'ACCEPT' => 'application/json' }
+    end
+
     context "User exists" do
       it 'is successful' do
         user = create(:user)
-        get api_v1_user_path(user)
+        get api_v1_user_path(user), headers: headers
         expect(response).to be_successful
       end
     end
 
     context "User does not exists" do
       it "is not found" do
+        get api_v1_user_path(50), headers: headers
+        expect(response.status).to eq 404
       end
     end
   end
