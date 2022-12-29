a0c7e0a36711215527762a7b4a9d94ed84e781ac
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-29 18:05:32 -0500

    formats controller and replaces the fetch request with axios

--- app/javascript/controllers/user_auth_controller.js
+++ app/javascript/controllers/user_auth_controller.js
@@ -1,47 +1,56 @@
-import { Controller } from "@hotwired/stimulus"
-import { enter, leave, toggle } from "el-transition";
+import { Controller } from "@hotwired/stimulus";
+import { enter, leave } from "el-transition";
 import axios from "axios";
-
-var FormData = require("form-data");
 
 // Connects to data-controller="user-auth"
 export default class extends Controller {
-  static targets = ['exitBtn', 'panelTitle', 'backBtn', 'emailInput', 'emailAlert', 'mailValidationForm', 'signInForm', 'emailAlertText', 'emailField']
+  static targets = [
+    "exitBtn",
+    "panelTitle",
+    "backBtn",
+    "emailInput",
+    "emailAlert",
+    "mailValidationForm",
+    "signInForm",
+    "emailAlertText",
+    "emailField",
+  ];
   connect() {
     console.log(axios);
   }
 
   submitForm() {
-    if(this.emailInputTarget.value == '') {
+    if (this.emailInputTarget.value == "") {
       this.emailAlertTextTarget.innerText = "Email is required.";
       enter(this.emailAlertTarget);
     } else {
-      fetch(
-        `http://localhost:3000/api/v1/users_by_email?email=${this.emailInputTarget.value}`
-      )
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
-        if(data.id) this.#displayForm(this.signInFormTarget)
+      axios.get("/api/v1/users_by_email", {
+        params: { email: this.emailInputTarget.value },
+        headers:{'ACCEPT': 'application/json'}
+      }).then((response) => {
+        this.#displayForm(this.signInFormTarget);
+      }).catch((response)=> {
+        console.log('failed response:', response);
+        console.log('Pending ot implement signup form');
       })
     }
   }
-  resetForm(){
-    this.emailFieldTarget.value = '';
-    this.emailInputTarget.value = '';
+  resetForm() {
+    this.emailFieldTarget.value = "";
+    this.emailInputTarget.value = "";
     leave(this.backBtnTarget)
-    .then(()=>leave(this.signInFormTarget))
-    .then(()=>leave(this.emailAlertTarget))
-    .then(()=>enter(this.exitBtnTarget))
-    .then(()=>enter(this.mailValidationFormTarget))
-    this.panelTitleTarget.innerText = 'Log in or sign up';
+      .then(() => leave(this.signInFormTarget))
+      .then(() => leave(this.emailAlertTarget))
+      .then(() => enter(this.exitBtnTarget))
+      .then(() => enter(this.mailValidationFormTarget));
+    this.panelTitleTarget.innerText = "Log in or sign up";
   }
-  #displayForm(formToDisplay){
+  #displayForm(formToDisplay) {
     this.emailFieldTarget.value = this.emailInputTarget.value;
     leave(this.exitBtnTarget)
-    .then(()=>leave(this.mailValidationFormTarget))
-    .then(()=> enter(this.backBtnTarget))
-    .then(()=>enter(formToDisplay))
+      .then(() => leave(this.mailValidationFormTarget))
+      .then(() => enter(this.backBtnTarget))
+      .then(() => enter(formToDisplay));
     this.panelTitleTarget.innerText = "Log in";
   }
 }
