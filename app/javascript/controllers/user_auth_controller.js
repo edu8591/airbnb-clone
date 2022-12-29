221827586fff82ae99f93458346d6d76e28d6f4f
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-28 21:34:11 -0500

    updates user_auth_controller to toggle between forms inside the modal

--- app/javascript/controllers/user_auth_controller.js
+++ app/javascript/controllers/user_auth_controller.js
@@ -4,21 +4,42 @@ import { enter, leave, toggle } from "el-transition";
 
 // Connects to data-controller="user-auth"
 export default class extends Controller {
-  static targets = ['exitBtn', 'panelTitle', 'backBtn', 'emailInput', 'emailAlert', 'mailValidationForm']
+  static targets = ['exitBtn', 'panelTitle', 'backBtn', 'emailInput', 'emailAlert', 'mailValidationForm', 'signInForm', 'emailAlertText', 'emailField']
   connect() {
-    console.log('connected to user_auth controller')
+    console.log(this.emailFieldTarget);
   }
+
   submitForm() {
     if(this.emailInputTarget.value == '') {
+      this.emailAlertTextTarget.innerText = "Email is required.";
       enter(this.emailAlertTarget);
     } else {
-      this.#toggleForms()
+      fetch(
+        `http://localhost:3000/api/v1/users_by_email?email=${this.emailInputTarget.value}`
+      )
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        if(data.id) this.#displayForm(this.signInFormTarget)
+      })
     }
   }
-  #toggleForms() {
-    this.panelTitleTarget.innerText = 'Log in'
-    toggle(this.exitBtnTarget)
-    toggle(this.backBtnTarget)
+  resetForm(){
+    this.emailFieldTarget.value = '';
+    this.emailInputTarget.value = '';
+    this.panelTitleTarget.innerText = 'Log in or sign up';
+    leave(this.backBtnTarget);
+    enter(this.exitBtnTarget);
+    enter(this.mailValidationFormTarget);
+    leave(this.signInFormTarget);
     leave(this.emailAlertTarget);
+  }
+  #displayForm(formToDisplay){
+    this.panelTitleTarget.innerText = "Log in";
+    leave(this.exitBtnTarget);
+    enter(this.backBtnTarget);
+    this.emailFieldTarget.value = this.emailInputTarget.value;
+    leave(this.mailValidationFormTarget)
+    enter(formToDisplay)
   }
 }
