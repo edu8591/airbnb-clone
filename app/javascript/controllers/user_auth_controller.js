6266fa1d6897b933ecded14abc88093972f18755
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-29 18:51:58 -0500

    Add axios pkg (#10)
    * pins axios with importmap and import it on js controller
    
    * formats controller and replaces the fetch request with axios
    
    * remove the api/v1/users_controller
    
    * add functions to user-auth js controller

--- app/javascript/controllers/user_auth_controller.js
+++ app/javascript/controllers/user_auth_controller.js
@@ -1,45 +1,65 @@
-import { Controller } from "@hotwired/stimulus"
-import { enter, leave, toggle } from "el-transition";
-
+import { Controller } from "@hotwired/stimulus";
+import { enter, leave } from "el-transition";
+import axios from "axios";
 
 // Connects to data-controller="user-auth"
 export default class extends Controller {
-  static targets = ['exitBtn', 'panelTitle', 'backBtn', 'emailInput', 'emailAlert', 'mailValidationForm', 'signInForm', 'emailAlertText', 'emailField']
+  static targets = [
+    "exitBtn",
+    "panelTitle",
+    "backBtn",
+    "emailInput",
+    "emailAlert",
+    "mailValidationForm",
+    "signInForm",
+    "emailAlertText",
+    "emailField",
+  ];
   connect() {
-    console.log(this.emailFieldTarget);
+    console.log(axios);
   }
 
   submitForm() {
-    if(this.emailInputTarget.value == '') {
-      this.emailAlertTextTarget.innerText = "Email is required.";
-      enter(this.emailAlertTarget);
+    if (this.emailInputTarget.value.length === 0) {
+      this.#emailAlert("Email is required.")
+    } else if (this.#invalidEmail(this.emailInputTarget.value)) {
+      this.#emailAlert("Enter a valid email.");
     } else {
-      fetch(
-        `http://localhost:3000/api/v1/users_by_email?email=${this.emailInputTarget.value}`
-      )
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
-        if(data.id) this.#displayForm(this.signInFormTarget)
+      axios.get("/api/v1/users_by_email", {
+        params: { email: this.emailInputTarget.value },
+        headers:{'ACCEPT': 'application/json'}
+      }).then(() => {
+        this.#displayForm(this.signInFormTarget);
+      }).catch((response)=> {
+        console.log('failed response:', response);
+        console.log('Pending ot implement signup form');
       })
     }
   }
-  resetForm(){
-    this.emailFieldTarget.value = '';
-    this.emailInputTarget.value = '';
+  resetForm() {
+    this.emailFieldTarget.value = "";
+    this.emailInputTarget.value = "";
     leave(this.backBtnTarget)
-    .then(()=>leave(this.signInFormTarget))
-    .then(()=>leave(this.emailAlertTarget))
-    .then(()=>enter(this.exitBtnTarget))
-    .then(()=>enter(this.mailValidationFormTarget))
-    this.panelTitleTarget.innerText = 'Log in or sign up';
+      .then(() => leave(this.signInFormTarget))
+      .then(() => leave(this.emailAlertTarget))
+      .then(() => enter(this.exitBtnTarget))
+      .then(() => enter(this.mailValidationFormTarget));
+    this.panelTitleTarget.innerText = "Log in or sign up";
   }
-  #displayForm(formToDisplay){
+  #displayForm(formToDisplay) {
     this.emailFieldTarget.value = this.emailInputTarget.value;
     leave(this.exitBtnTarget)
-    .then(()=>leave(this.mailValidationFormTarget))
-    .then(()=> enter(this.backBtnTarget))
-    .then(()=>enter(formToDisplay))
+      .then(() => leave(this.mailValidationFormTarget))
+      .then(() => enter(this.backBtnTarget))
+      .then(() => enter(formToDisplay));
     this.panelTitleTarget.innerText = "Log in";
+  }
+  #invalidEmail(email){
+    // checks validity of email
+    return !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email));
+  }
+  #emailAlert(message) {
+    this.emailAlertTextTarget.innerText = message;
+    enter(this.emailAlertTarget);
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show]
       get 'users_by_email', to: 'users_by_emails#show', as: 'users_by_email'
     end
   end
