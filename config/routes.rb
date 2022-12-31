d02a93b19504f0f5cfeffbc911c505ed15c642b9
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-31 16:46:10 -0500

    removes sign_in and sign_up route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[registrations sessions]
+  as :user do
+    # registrations
+    get 'users/cancel', to: 'devise/registrations#cancel', as: 'cancel_user_registration'
+    get 'users/edit', to: 'devise/registrations#edit', as: 'edit_user_registration'
+    patch 'users', to: 'devise/registrations#update', as: 'user_registration'
+    put 'users', to: 'devise/registrations#update', as: ''
+    delete 'users', to: 'devise/registrations#destroy', as: ''
+    post 'users', to: 'devise/registrations#create', as: ''
+    # sessions
+    post '/users/sign_in', to: 'devise/sessions#create', as: 'user_session'
+    delete '/users/sign_out', to: 'devise/sessions#destroy', as: 'destroy_user_session'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
