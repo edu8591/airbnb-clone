ac6efa8e2c851347cb03cc28325d2f09fd317cf0
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-12-31 17:38:54 -0500

    creates the property model

---
+++ spec/factories/properties.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :property do
+    title { "MyString" }
+    headline { "MyString" }
+    description { "MyText" }
+    city { "MyString" }
+    state { "MyString" }
+    country { "MyString" }
+  end
+end
