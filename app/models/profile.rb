d4cd1a316a7c9aa7def5c9c8514b3beb655dc0e1
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-01-06 19:04:55 -0500

    add logic to property model, add callback to user to create property after user creation, adds user tests

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,15 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  geocoded_by :address, if: -> { address.present? && :address_changes? }
+
+  def address
+    [street_1, street_2, city, state, country].compact.join(', ')
+  end
+
+  private
+
+  def address_changes?
+    street_1_changed? || street_2_changed? || city_changed? || state_changed? || country_changed?
+  end
 end
