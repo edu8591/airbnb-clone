77b85aeb1077e59e13889b0541d0805ca24c0b44
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-01-24 16:27:17 -0500

    add reviews on seeds, desplay review score on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 10.times do |i|
+  puts "Creating property # #{i + 1}"
   property = Property.create!(
     name: Faker::Lorem.unique.words(number: 2).join(' '),
     headline: Faker::Lorem.sentence,
@@ -16,8 +17,16 @@
     country: 'United States',
     price: (100..250).to_a.sample
   )
+  puts 'Adding images to property'
   10.times do |b|
+    puts "Adding image # #{b + 1}"
     property.images.attach(io: File.open(Rails.root.join('db', 'sample', 'images', "property#{i + 1}", "#{b}.png")),
                            filename: "#{property.name}#{b}")
   end
+  puts 'Creating reviews'
+  (1..5).to_a.sample.times do
+    Review.create(title: Faker::Lorem.unique.word, body: Faker::Lorem.paragraph,
+                  rating: (1..5).to_a.sample, reviewable: property)
+  end
+  puts 'Property created!'
 end
